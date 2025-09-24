69467b99cbd0d4bbcb13bcb5013a84e59872e77a
monniiaa <sofielarsen2411@gmail.com>
2025-09-24 17:54:30 +0200

    s

--- path_planning.py
+++ path_planning.py
@@ -34,7 +34,7 @@ rrt = robot_RRT(
     path_resolution=path_res,
     )
 
-path =rrt.planning()
+path, distances  =rrt.planning()
 
 
 if path is None:
@@ -45,6 +45,6 @@ else:
     print(f"path: {path}")
 
     print(f"Smoothedpath: {smoothed_path}")
-    cal_arlo.follow_path(smoothed_path)
+    cal_arlo.follow_path(smoothed_path, distances)
     rrt.draw_graph(smoothed_path, name = "1")
     print("found path")
