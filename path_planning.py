51c7781e219b94e1bde6455fd89b2d1b25b8343a
monniiaa <sofielarsen2411@gmail.com>
2025-09-24 14:29:20 +0200

    s

--- path_planning.py
+++ path_planning.py
@@ -10,7 +10,7 @@ cam = CameraUtils()
 cam.start_camera()
 aruco = ArucoUtils()
 landmark_utils = LandmarkUtils(cam, aruco, cube_side=0.05, robot_radius=0.1)
-grid_map = LandmarkOccupancyGrid(low=(-2,-2), high=(2, 2), res=0.05)
+grid_map = LandmarkOccupancyGrid(low=(-3,-3), high=(3, 3), res=0.05)
 cal_arlo = CalibratedRobot()
 
 landmarks = landmark_utils.map_landmarks()
@@ -38,11 +38,11 @@ path =rrt.planning()
 
 print(f"path: {path}")
 
-rrt.draw_graph(path)
 
 #cal_arlo.follow_path(path)
 
 if path is None:
     print("Cannot find path")
 else:
+    rrt.draw_graph(path)
     print("found path")
--- robot_model.py
+++ robot_model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class RobotModel:
-    def __init__(self, robot_radius = 0.25):
+    def __init__(self, robot_radius = 0.225):
         self.robot_radius = robot_radius
         
     def dyn(self, from_pos, to_pos, n_steps):
