1d5038cb7978f335412d13faa6948394568e6b03
monniiaa <sofielarsen2411@gmail.com>
2025-09-24 11:34:33 +0200

    s

--- path_planning.py
+++ path_planning.py
@@ -7,7 +7,7 @@ cam = CameraUtils()
 cam.start_camera()
 aruco = ArucoUtils()
 landmark_utils = LandmarkUtils(cam, aruco, cube_side=0.05, robot_radius=0.1)
-grid_map = LandmarkOccupancyGrid(low=(-15,-15), high=(15, 15), res=0.05)
+grid_map = LandmarkOccupancyGrid(low=(-10,-10), high=(10, 10), res=0.05)
 
 landmarks = landmark_utils.map_landmarks()
 print(f"landmarks detected: {landmarks}")
@@ -18,7 +18,7 @@ landmarks_for_grid = [(pos[0], pos[1], landmark_radius) for _, pos in landmarks]
 grid_map.add_landmarks(landmarks_for_grid)
 
 robot_pos = (0.0, 0.0)
-robot_radius = 0.225
+robot_radius = 0.25
 
 plt.figure(figsize=(5,5))
 grid_map.draw_map( robot_radius=robot_radius)
