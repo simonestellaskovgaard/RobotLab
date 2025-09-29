9d778b105aeda2719575d7d34d742dbe24eb7d4d
monniiaa <sofielarsen2411@gmail.com>
2025-09-29 15:36:21 +0200

    s

--- RobotUtils/CameraUtils.py
+++ RobotUtils/CameraUtils.py
@@ -12,7 +12,7 @@ class CameraUtils:
     OpenCV.
     """
     ## Camera calibration
-    def __init__(self, width=3280, height=2464, fx=2569, fy=2569, cx=None, cy=None, fps = 30):
+    def __init__(self, width=1640, height=1232, fx=2569, fy=2569, cx=None, cy=None, fps = 30):
         self.picam2 = None
         self.width = width #resultion
         self.height = height #resulution
@@ -33,8 +33,8 @@ class CameraUtils:
         self.picam2 = Picamera2()
         pprint.pprint(self.picam2.camera_controls)
         config = self.picam2.create_video_configuration(
-            main={"size": (self.width, self.height), "format": "RGB888"},
-            controls = { "ScalerCrop": (0,0,3280,2464)}
+            main={"size": (self.width, self.height), "format": "RGB888"}
+            #controls = { "ScalerCrop": (0,0,3280,2464)}
         )
           # Apply fixed fps
         frame_time = int(1e6 / self.fps)
